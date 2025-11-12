19611409cfd2eb2b733705044d844aa2d19c5069
Marcos Magueta <maguetamarcos@gmail.com>
2025-11-12 00:23:20 -0300

    fix startup crash

--- src/main.erl
+++ src/main.erl
@@ -1,7 +1,26 @@
 -module(main).
--export([fibonacci/1]).
+-behaviour(application).
 
-fibonacci(0) -> 0;
-fibonacci(1) -> 1;
-fibonacci(N) ->
-    fibonacci(N-2) + fibonacci(N-1).
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    %% Start Mnesia (or other children) here
+    io:format("Starting app...~n"),
+
+    %% Ensure Mnesia is started
+    mnesia:start(),
+
+    {ok, self()}.
+
+stop(_State) ->
+    io:format("Stopping app...~n"),
+    ok.
+
+%% setup() ->
+%%     mnesia:create_table(attributes, [{type, set},
+%% 				 {attributes, [name, value, type]},
+%% 				 {ram_copies, [node()]},
+%% 				 {index, [name]}
+%% 				]).
+
+
