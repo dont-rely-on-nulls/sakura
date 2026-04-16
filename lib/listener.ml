0ea0ba60fe42b57fbd147540d0b214a680fbdf94
Estevan Castilho <tevo@riajuu.net>
2026-04-16 01:42:38 +0000

    State transitions, some serialization
    This should at least bring us back to feature parity with the old
    server, although the network protocol is still likely to change

--- lib/listener.ml
+++ lib/listener.ml
@@ -12,38 +12,13 @@ module StringMap = BatMap.Make(StringKey)
 module Make : LISTENER =
   functor (T : Transport.TRANSPORT)(S : Management.Physical.S with type error = string) -> struct
     module type SubS = Sublanguage.S with type storage = S.t
-
-    (* let dispatch = *)
-    (*   Dispatch.create [ *)
-    (*       (module Drl.Make(S) : Dispatch.SubS); *)
-    (*       (module Ddl.Make(S) : Dispatch.SubS); *)
-    (*       (module Dml.Make(S) : Dispatch.SubS); *)
-    (*       (module Icl.Make(S) : Dispatch.SubS); *)
-    (*       (module Dcl.Make(S) : Dispatch.SubS); *)
-    (*       (module Scl.Make(S) : Dispatch.SubS); *)
-    (*     ] *)
+    module BranchOps = Management.Branch.Make (S)
+    module AlgebraOps = Algebra.Make (S)
 
     let read_command input =
       try
         Ok (Sexplib.Sexp.input_sexp input)
       with e -> Error (Error.SyntaxError (Printexc.to_string e))
-
-    (* let execute_command storage db_ref cmd = *)
-    (*   let db = !db_ref in *)
-    (*   let h = db.Management.Database.hash in *)
-    (*   let name = db.Management.Database.name in *)
-    (*   let ok = ok_response storage name in *)
-    (*   let err = error_response storage name in *)
-    (*   match Dispatch.execute dbms_dispatch storage db cmd with *)
-    (*   | Ok (Sublanguage.Query rel) -> *)
-    (*       relation_to_sexp storage name h rel default_limit *)
-    (*   | Ok (Sublanguage.Transition (new_db, msg)) -> *)
-    (*       db_ref := new_db; *)
-    (*       advance_head_branch storage new_db.Management.Database.hash; *)
-    (*       ok new_db.Management.Database.hash msg *)
-    (*   | Ok (Sublanguage.Cursor { cursor_id; rows; has_more }) -> *)
-    (*       cursor_to_sexp storage name h cursor_id rows has_more *)
-    (*   | Error e -> err h (Dispatch.sexp_of_dispatch_error e) *)
 
     let sublanguages =          (* should this go here? *)
       List.fold_right
@@ -75,14 +50,109 @@ module Make : LISTENER =
          |> fmap (execute storage db expr)
       | s -> Error (Error.MalformedExpression s)
 
-    let perform db_head old_db = function
-      | Sublanguage.Transition (new_db, msg) ->
-         if Atomic.compare_and_set db_head old_db new_db
-         then Ok ()
-         else Error (Error.Conflict { old_db; new_db })
-      | _ -> Ok ()
+    let advance_head_branch storage new_hash =
+      match BranchOps.get_head storage with
+      | Ok (Some branch_name) ->
+         ignore (BranchOps.update_tip storage ~name:branch_name ~tip:new_hash)
+      | _ -> ()
 
-    let handle_client connection db_head storage =
+    let perform storage db_head old_db result =
+      match result with
+      | Sublanguage.Transition (new_db, _) ->
+         if Atomic.compare_and_set db_head old_db new_db
+         then (advance_head_branch storage new_db.hash; Ok result)
+         else Error (Error.Conflict { old_db; new_db })
+      | _ -> Ok result
+
+    let get_branch storage =
+      match BranchOps.get_head storage with Ok (Some name) -> name | _ -> "--"
+
+    let current_limit = 16      (* TODO: make the limit per-connection? *)
+
+    let materialize_generator gen limit =
+      let rec go gen pos acc count =
+        if count >= limit then (List.rev acc, true)
+        else
+          match gen (Some pos) with
+          | Generator.Done -> (List.rev acc, false)
+          | Generator.Error _ -> (List.rev acc, false)
+          | Generator.Value (t, next) -> (
+            let mat =
+              match t with
+              | Tuple.Materialized m -> Some m
+              | Tuple.NonMaterialized _ -> None
+            in
+            match mat with
+            | None -> go next (pos + 1) acc count
+            | Some m -> go next (pos + 1) (m :: acc) (count + 1))
+      in
+      go gen 0 [] 0
+
+    let materialize_relation storage (rel : Relation.t) limit =
+      let gen = AlgebraOps.to_generator storage rel in
+      materialize_generator gen limit
+
+    let tuple_to_sexp (t : Tuple.materialized) =
+      let open Sexplib.Sexp in
+      List (Tuple.AttributeMap.bindings t.Tuple.attributes
+            |> List.map (fun (k, attr) -> List [ Atom k; Conventions.AbstractValue.sexp_of_t attr.Attribute.value ]))
+
+    (* TODO: does our network protocol make sense? *)
+    let serialize storage =
+      let open Sexplib.Sexp in
+      function
+      | Error e -> List [ Atom "error"; (Error.sexp_of_error e) ]
+      (* TODO: forsake the global cursor register in favour of per-connection bookkeeping *)
+      | Ok (Sublanguage.Cursor { cursor_id; rows; has_more }) ->
+         let row_sexps = List.map tuple_to_sexp rows in
+         let rows_sexp = List row_sexps in
+         List [ Atom "cursor";
+                List [ Atom "id"; Atom cursor_id ];
+                List [ Atom "rows"; rows_sexp ];
+                List [ Atom "row_count"; Atom (string_of_int (List.length row_sexps)) ];
+                List [ Atom "has_more"; Atom (string_of_bool has_more) ];
+                (* List [ Atom "db_hash"; Atom (short_hash db_hash) ]; *)
+                (* List [ Atom "db_name"; Atom db_name ]; *)
+                List [ Atom "branch"; Atom (get_branch storage) ] ]
+      | Ok (Sublanguage.Query rel) ->
+         let tuples, truncated = materialize_relation storage rel current_limit in
+         let schema_sexp = List (List.map (fun (a, d) -> List [ Atom a; Atom d ]) rel.Relation.schema) in
+         let rows_sexp = List (List.map tuple_to_sexp tuples) in
+         List [ Atom "relation";
+                List [ Atom "name"; Atom rel.Relation.name ];
+                List [ Atom "schema"; schema_sexp ];
+                List [ Atom "rows"; rows_sexp ];
+                List [ Atom "row_count"; Atom (string_of_int (List.length tuples)) ];
+                List [ Atom "truncated"; Atom (string_of_bool truncated) ];
+                (* List [ Atom "db_hash"; Atom db.hash ]; *)
+                (* List [ Atom "db_name"; Atom db.name ]; *)
+                List [ Atom "branch"; Atom (get_branch storage) ] ]
+      | Ok (Sublanguage.Transition (new_db, message)) ->
+         List [ Atom "ok";
+                List [ Atom "message"; Atom message ];
+                List [ Atom "db_hash"; Atom new_db.hash ];
+                List [ Atom "db_name"; Atom new_db.name ];
+                (* TODO: `serialize' shouldn't need access to the storage layer *)
+                List [ Atom "branch"; Atom (get_branch storage) ] ]
+
+    let print_with_time str =   (* TODO: a proper logger *)
+      let now = Unix.gettimeofday () in
+      let tm = Unix.localtime now in
+      let orange = "\027[38;5;208m" in
+      let reset = "\027[0m" in
+      let formatted_time =
+        Printf.sprintf "%04d-%02d-%02d %02d:%02d:%02d" (tm.Unix.tm_year + 1900)
+          (tm.Unix.tm_mon + 1) tm.Unix.tm_mday tm.Unix.tm_hour tm.Unix.tm_min
+          tm.Unix.tm_sec
+      in
+      print_endline @@ Printf.sprintf "%s[%s]%s %s" orange formatted_time reset str
+
+    let output_response output sexp =
+      let response = Sexplib.Sexp.to_string sexp in
+      output_string output response;
+      print_with_time response
+
+    let handle_client connection db_head (storage : S.t) =
       let input = T.input connection in
       let output = T.output connection in
       try
@@ -90,9 +160,9 @@ module Make : LISTENER =
           let db = Atomic.get db_head in
           read_command input
           |> fmap (execute_command storage db)
-          |> fmap (perform db_head db)
-          (* |> serialize_result *)
-          (* |> output_response output *)
+          |> fmap (perform storage db_head db)
+          |> serialize storage
+          |> output_response output
           |> ignore
         done
       with End_of_file -> ()
